f29365fcaa5dfa0844e81c6b99559ca94bc20c97
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-11-13 16:02:17 -0600

    check numeral palindrome exercise

--- js/chris-exercise.js
+++ js/chris-exercise.js
@@ -10,7 +10,7 @@ function equal(a, b, c) {
     }
 }
 
-console.log(equal(2, 3, 2));
+// console.log(equal(2, 3, 2));
 
 //Exercise #2
 
@@ -34,4 +34,49 @@ function xo(str) {
     }
 }
 
-console.log(xo("sammy"));
+// console.log(xo("sammy"));
+
+
+let numExample = 11211230;
+let numExample2 = 13001120;
+let numExample3 = 23336014;
+let resultingNumber;
+
+
+let descendentOfNumber = (number) => {
+    let numberToString = number.toString();
+    let arr = numberToString.split("");
+    let bucketArr = [];
+    for(let i = 0; i< arr.length;i +=2){
+        bucketArr.push(parseInt(arr[i]) + parseInt(arr[i + 1]));
+    }
+    return parseInt(bucketArr.join(""));
+};
+
+function palindromeDescendant(num){
+    let result = false;
+    resultingNumber = descendentOfNumber(num);
+    // console.log(resultingNumber);
+    // console.log(checkPalidrone(resultingNumber));
+    while(resultingNumber.toString().split("").length > 1){
+        // console.log("Length: " + resultingNumber.toString().split("").length);
+        resultingNumber = descendentOfNumber(resultingNumber);
+        if(resultingNumber.toString().split("").length> 1){
+            // console.log(checkPalidrone(resultingNumber));
+            result = checkPalidrone(resultingNumber);
+        }
+    }
+    return result;
+}
+
+function checkPalidrone(str){
+    return str.toString() === str.toString().split('').reverse().join('');
+}
+
+console.log(palindromeDescendant(numExample));
+console.log(palindromeDescendant(numExample2));
+console.log(palindromeDescendant(numExample3));
+console.log(palindromeDescendant(1122));
+
+
+
