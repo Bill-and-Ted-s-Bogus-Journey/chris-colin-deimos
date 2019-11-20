8a0ff8977d0b5a3bcfec5b4cf9a02b9489de02e3
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-11-20 16:59:13 -0600

    morse code question

--- READ_ME.md
+++ READ_ME.md
@@ -115,3 +115,30 @@ Source: https://edabit.com/challenge/
 Exercise done by: Chris and Colin
 Bonus Exercises for map filter and reduce
 ----
+
+## Pod#2 Week#2
+
+### Chris' Week 2 Exercises
+
+### Function Factory
+Create a function that takes a "base number" as an argument. This function should return another function which takes a new argument, and returns the sum of the "base number" and the new argument.
+
+// Calling makePlusFunction(5) returns a new function that takes an input,
+// and returns the result when adding 5 to it.
+
+const plusFive = makePlusFunction(5)
+
+plusFive(2) ➞ 7
+
+plusFive(-8) ➞ -3
+
+// Calling makePlusFunction(10) returns a new function that takes an input,
+// and returns the result when adding 10 to it.
+
+const plusTen = makePlusFunction(10)
+
+plusTen(0) ➞ 10
+
+plusTen(188) ➞ 198
+
+plusFive(plusTen(0)) ➞ 15
--- js/chris-exercise.js
+++ js/chris-exercise.js
@@ -81,10 +81,79 @@ function checkPalidrone(str){
     return str.toString() === str.toString().split('').reverse().join('');
 }
 
-console.log(palindromeDescendant(numExample));
-console.log(palindromeDescendant(numExample2));
-console.log(palindromeDescendant(numExample3));
-console.log(palindromeDescendant(332233));
+// console.log(palindromeDescendant(numExample));
+// console.log(palindromeDescendant(numExample2));
+// console.log(palindromeDescendant(numExample3));
+// console.log(palindromeDescendant(332233));
 
+///////////Week 2 Exercises///////////
 
+ //Exercise 1//
 
+ function makePlusFunction(baseNum) {
+    return function(num){
+        return num + baseNum;
+    };
+}
+const plusTen = makePlusFunction(10);
+const plusFive = makePlusFunction(5);
+
+console.log(plusTen(20));
+console.log(plusFive(112));
+
+//Exercise 2//
+
+function decodeMorse(morseCode) {
+
+    var ref = {
+        '.-':     'a',
+        '-...':   'b',
+        '-.-.':   'c',
+        '-..':    'd',
+        '.':      'e',
+        '..-.':   'f',
+        '--.':    'g',
+        '....':   'h',
+        '..':     'i',
+        '.---':   'j',
+        '-.-':    'k',
+        '.-..':   'l',
+        '--':     'm',
+        '-.':     'n',
+        '---':    'o',
+        '.--.':   'p',
+        '--.-':   'q',
+        '.-.':    'r',
+        '...':    's',
+        '-':      't',
+        '..-':    'u',
+        '...-':   'v',
+        '.--':    'w',
+        '-..-':   'x',
+        '-.--':   'y',
+        '--..':   'z',
+        '.----':  '1',
+        '..---':  '2',
+        '...--':  '3',
+        '....-':  '4',
+        '.....':  '5',
+        '-....':  '6',
+        '--...':  '7',
+        '---..':  '8',
+        '----.':  '9',
+        '-----':  '0',
+    };
+
+    return morseCode
+        .split('   ')
+        .map(
+            word => word
+                .split(' ')
+                .map(
+                    el => ref[el]
+                ).join('')
+        ).join(' ');
+}
+
+var decoded = decodeMorse("- .... .  -. .- -- .  .. ...  -.-. .... .-. .. ...");
+console.log(decoded);
